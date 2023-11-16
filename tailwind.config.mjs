5024c147cfc46cd533dfb7c777ba95df40512af6
Abhushan A. Joshi <49617450+abhu-A-J@users.noreply.github.com>
2023-11-16 10:43:21 +0530

    chore: base layout setup and added a home page (#2)
    * chore: base layout setup and added a home page
    
    * fix: some scrollbar customziation
    
    * chore: add blurring effect on top of page
    
    * fix: user selection and pointer events
    
    * chore: add a navbar to page with links to home and blog page
    
    * fix: typos on the page

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,22 @@
 export default {
 	content: ['./src/**/*.{astro,md,mdx,ts,tsx}'],
 	theme: {
-		extend: {}
+		colors: {
+			transparent: 'transparent',
+			current: 'currentColor',
+			'gray-1': 'var(--colors-gray-1)',
+			'gray-2': 'var(--colors-gray-2)',
+			'gray-3': 'var(--colors-gray-3)',
+			'gray-4': 'var(--colors-gray-4)',
+			'gray-5': 'var(--colors-gray-5)',
+			'gray-6': 'var(--colors-gray-6)',
+			'gray-7': 'var(--colors-gray-7)',
+			'gray-8': 'var(--colors-gray-8)',
+			'gray-9': 'var(--colors-gray-9)',
+			'gray-10': 'var(--colors-gray-10)',
+			'gray-11': 'var(--colors-gray-11)',
+			'gray-12': 'var(--colors-gray-12)'
+		}
 	},
 	plugins: []
 };
