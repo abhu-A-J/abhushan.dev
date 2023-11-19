c0b18f82578ab8f96aedece22a0fafe1d70913e8
abhu-A-J <abhushanadhikarijoshi@gmail.com>
2023-11-19 12:17:22 +0530

    chore: update with better titles on anchor

--- src/components/react/blog/blog-listing/index.tsx
+++ src/components/react/blog/blog-listing/index.tsx
@@ -13,21 +13,26 @@ type Props = {
 function BlogListing({ listings }: Props) {
 	return (
 		<ul className="flex flex-col gap-5">
-			{listings.map((listing) => (
-				<li>
-					<div>
-						<a href={getPathFromSlug(listing.slug)} className="text-gray-12 underline w-fit">
-							<p className="text-current">{listing.title}</p>
-						</a>
-						<p className="mt-1 flex flex-wrap items-center gap-2 text-sm">
-							<time dateTime={new Date(listing.publishedDate).toDateString()}>
-								{new Date(listing.publishedDate).toDateString()}
-							</time>
-							{listing.seriesLength && <span> - ({listing.seriesLength} part series)</span>}
-						</p>
-					</div>
-				</li>
-			))}
+			{listings.map((listing) => {
+				const anchorTitle = listing.seriesLength
+					? `Explore the complete series: ${listing.title}`
+					: `Read about ${listing.title}`;
+				return (
+					<li>
+						<div>
+							<a href={getPathFromSlug(listing.slug)} className="w-fit text-gray-12 underline" title={anchorTitle}>
+								<p className="text-current">{listing.title}</p>
+							</a>
+							<p className="mt-1 flex flex-wrap items-center gap-2 text-sm">
+								<time dateTime={new Date(listing.publishedDate).toDateString()}>
+									{new Date(listing.publishedDate).toDateString()}
+								</time>
+								{listing.seriesLength && <span> - ({listing.seriesLength} part series)</span>}
+							</p>
+						</div>
+					</li>
+				);
+			})}
 		</ul>
 	);
 }
