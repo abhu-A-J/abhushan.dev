00d7273a16e13cc4d615604d61b48a533f607549
abhu-A-J <abhushanadhikarijoshi@gmail.com>
2023-11-27 11:20:38 +0530

    feat: add back action on blogs

--- src/components/react/blog/blog-header/index.tsx
+++ src/components/react/blog/blog-header/index.tsx
@@ -1,7 +1,7 @@
 type Props = {
 	title: string;
 	publishedDate: Date;
-	totalViews?: number;
+	totalViews?: string;
 };
 
 function BlogHeader({ title, publishedDate, totalViews }: Props) {
@@ -9,7 +9,7 @@ function BlogHeader({ title, publishedDate, totalViews }: Props) {
 
 	return (
 		<header className="mb-8 border-b-2 border-gray-6 pb-3">
-			<h1 className="text-2xl md:text-3xl font-bold">{title}</h1>
+			<h1 className="mt-2 text-2xl font-bold md:text-3xl">{title}</h1>
 			<p className="mt-2 flex flex-wrap items-center justify-between gap-2 text-sm">
 				<time dateTime={publishedDateString}>{publishedDateString}</time>
 				{totalViews && <span>{totalViews}</span>}
