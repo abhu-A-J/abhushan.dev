1e12b8b98609d1bbbe5de288db2e8b9582581674
abhu-A-J <abhushanadhikarijoshi@gmail.com>
2023-11-28 11:19:37 +0530

    chore: update the componenys block mapping

--- src/components/react/blog/content-blocks/index.tsx
+++ src/components/react/blog/content-blocks/index.tsx
@@ -4,11 +4,13 @@ import Heading from './heading';
 import Paragraph from './paragraph';
 import Divider from './divider';
 
+type PolymorphicHeadingProps = Omit<ComponentProps<typeof Heading>, 'as'>;
+
 export const components = {
-	h2: (props: Omit<ComponentProps<typeof Heading>, 'as'>) => <Heading as="h2" {...props} />,
-	h3: (props: Omit<ComponentProps<typeof Heading>, 'as'>) => <Heading as="h3" {...props} />,
-	h4: (props: Omit<ComponentProps<typeof Heading>, 'as'>) => <Heading as="h4" {...props} />,
-	h5: (props: Omit<ComponentProps<typeof Heading>, 'as'>) => <Heading as="h5" {...props} />,
+	h2: (props: PolymorphicHeadingProps) => <Heading as="h2" {...props} />,
+	h3: (props: PolymorphicHeadingProps) => <Heading as="h3" {...props} />,
+	h4: (props: PolymorphicHeadingProps) => <Heading as="h4" {...props} />,
+	h5: (props: PolymorphicHeadingProps) => <Heading as="h5" {...props} />,
 	p: Paragraph,
 	hr: Divider
 };
